55453011c4c6c0a9c92213848db54513f6f4c2de
Abishake01 <abishake381@gmail.com>
2024-03-05 00:17:33 +0530

    Del

--- Hello/hello_world/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render,HttpResponse
-
-# Create your views here.
-
-def show(request):
-    return HttpResponse('Its Working...')
-def home(request):
-    return HttpResponse('<h1>Home </h1>')
