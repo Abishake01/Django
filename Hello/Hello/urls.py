453c228c1fec461d4a52e54e5f64eb67636abbdb
Abishake01 <abishake381@gmail.com>
2024-03-05 00:16:33 +0530

    Modify

--- Hello/Hello/urls.py
+++ Hello/Hello/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('hello_world.urls'))
+   
+    path('',include("My_app.urls")),
  
 ]
