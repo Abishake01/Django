e61fb8a4d169ab85d6ab145c8f8eaa3dc85fd82a
Abishake01 <abishake381@gmail.com>
2024-03-12 20:46:13 +0530

    forms

--- Project/myfirstapps/form.py
+++ Project/myfirstapps/form.py
@@ -1,4 +1,4 @@
 from django import forms
 class Feedback(forms.Form):
-    title=forms.CharField(label='Title',max_length=50,widget=forms.TextInput)
-    subject=forms.CharField(label='Subject',max_length=150,widget=forms.Textarea)
+    title=forms.CharField(label='Title',max_length=50,widget=forms.TextInput(attrs={'class:form-control'}))
+    subject=forms.CharField(label='Subject',max_length=150,widget=forms.Textarea(attrs={'class:form-control'}))
