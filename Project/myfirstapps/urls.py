b06fa43caaf1552973bd8b9ef4fd3962e2d3e37d
Abishake01 <abishake381@gmail.com>
2024-03-12 21:21:47 +0530

    form

--- Project/myfirstapps/urls.py
+++ Project/myfirstapps/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('second',views.sec,name='Secondpage'),
     path('image/<str:imagename>',views.image,name='My image Page'),
     path('submitform',views.submitform,name='submitform'),
-    path('form',views.form,name='form')
+    path('form',views.form,name='form'),
+    
 ]
