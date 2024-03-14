b0133e56507ff03d2efb3f17bc104bedfabf4196
Abishake01 <abishake381@gmail.com>
2024-03-14 16:46:20 +0530

    Bootstrp added

--- Project/myfirstpro/urls.py
+++ Project/myfirstpro/urls.py
@@ -17,9 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from django.conf.urls.static import static
+from myfirstpro import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
     path('',include('myfirstapps.urls')),
     
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
